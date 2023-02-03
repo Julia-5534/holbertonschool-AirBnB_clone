c39f62936c46be39a7a5350cdd7c0f5a630bc821
Julia Bullard <5534@holbertonstudents.com>
2023-02-03 14:14:00 -0600

    Test

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,28 @@
 This is a test for doing github pull requests
+
+#!/usr/bin/python3
+"""Base Model for AirBnB Clone"""
+
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Base Model Stuff"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_representation = self.__dict__.copy()
+        dict_representation["__class__"] = self.__class__.__name__
+        dict_representation["created_at"] = self.created_at.isoformat()
+        dict_representation["updated_at"] = self.updated_at.isoformat()
+        return dict_representation
