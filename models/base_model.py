a50541043a363d1eef537814c8dc4c20314140f9
Julia Bullard <5534@holbertonstudents.com>
2023-02-03 14:44:02 -0600

    I hate that Holberton doesn't TEACH us anything.

--- models/base_model.py
+++ models/base_model.py
@@ -26,3 +26,33 @@ class BaseModel:
         dict_representation["created_at"] = self.created_at.isoformat()
         dict_representation["updated_at"] = self.updated_at.isoformat()
         return dict_representation
+
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_representation = self.__dict__.copy()
+        dict_representation["__class__"] = self.__class__.__name__
+        dict_representation["created_at"] = self.created_at.isoformat()
+        dict_representation["updated_at"] = self.updated_at.isoformat()
+        return dict_representation
